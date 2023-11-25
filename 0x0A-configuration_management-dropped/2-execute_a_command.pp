8467ceea9b8a81612f28678c2394ee9a60d6036c
Robert Amoah <mr_robertamoah@yahoo.com>
2023-11-25 04:02:22 +0000

    removed a dropped directory

--- 0x0A-configuration_management-dropped/2-execute_a_command.pp
+++
@@ -1,7 +0,0 @@
-# a manifest that kills a process named killmenow
-
-exec {'kill_killmenow':
-    command => 'pkill killmenow',
-    onlyif  => 'pgrep killmenow',
-    path    => '/usr/bin:/bin',
-}

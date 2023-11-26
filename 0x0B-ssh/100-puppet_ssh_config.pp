231bcc77bef02fd126e8db9dec358c443ed1fb7a
Robert Amoah <mr_robertamoah@yahoo.com>
2023-11-26 12:24:29 +0000

    made some changes to advanced task after installing puppet on server

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,11 +1,10 @@
 file {'/etc/ssh/ssh_config':
     ensure  => 'present',
     content => "
-    Host *
-      IdentityFile ~/.ssh/school
-      PasswordAuthentication no
-      SendEnv LANG LC_*
-      HashKnownHosts yes
-      GSSAPIAuthentication yes
-  "
+Host *
+  IdentityFile ~/.ssh/school
+  PasswordAuthentication no
+  SendEnv LANG LC_*
+  HashKnownHosts yes
+  GSSAPIAuthentication yes"
 }

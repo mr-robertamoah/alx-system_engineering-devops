705cb2e006b394747e770c249b5f12383dedc001
Robert Amoah <mr_robertamoah@yahoo.com>
2024-01-02 08:24:52 +0000

    done with task 0 of api project

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+
+"""
+script returns information about Employee TODO list
+"""
+
+
+import requests
+import sys
+import json
+
+userId = sys.argv[1]
+userUrl = f"https://jsonplaceholder.typicode.com/users?id={userId}"
+todoUrl = f"https://jsonplaceholder.typicode.com/todos?userId={userId}"
+
+res = requests.get(userUrl)
+user = res.json()[0]
+res = requests.get(todoUrl)
+todos = res.json()
+
+completed = len(list(filter(lambda x: x["completed"] is True, todos)))
+print(f"Employee {user['name']} is done with tasks ({completed}/20):")
+for todo in todos:
+    print(f"\t {todo['title']}")
---
+++ 0x15-api/README.md
@@ -0,0 +1,9 @@
+# SYSTEM ENGINEERING DEVOPS
+
+## MySQL
+
+---
+Objectives of the project is to know:
+- What is an API
+- What is REST API
+- What are microservices
